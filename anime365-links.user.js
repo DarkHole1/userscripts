02a804cbe4b88069d15371b33588785cd9198014
Dark Hole <dark.hole1@yandex.ru>
2024-07-05 14:18:33 +0300

    Create anime365-links.user.js

---
+++ anime365-links.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name        Anime365 Link
+// @namespace   Dark Hole's Scripts
+// @match       https://shikimori.one/*
+// @grant       GM_xmlhttpRequest
+// @version     0.1.0
+// @author      Dark Hole
+// @description Add link to Anime365 under picture
+// ==/UserScript==
+
+var regexParser = /^https:\/\/shikimori\.(?:one|me)\/animes\/(\d+)[^\/]*$/;
+
+function tryInsertButton(url) {
+  var match = url.match(regexParser);
+  if (!match) {
+    return;
+  }
+  var id = match[1];
+
+  var poster = document.getElementsByClassName('c-image')[0];
+  if(!poster) {
+    return;
+  }
+  if (poster.getElementsByClassName('anime365-link').length > 0) {
+    return;
+  }
+
+  GM_xmlhttpRequest({
+    url: 'https://smotret-anime.online/api/series/?myAnimeListId=' + id,
+    method: 'GET',
+    responseType: 'json',
+    onload: function(res) {
+      if (res.response.data.length <= 0) {
+        return;
+      }
+
+      var btn = document.createElement('a');
+      btn.className = "b-link_button anime365-link";
+      btn.text = 'Anime365';
+      btn.href = res.response.data[0].url;
+      poster.appendChild(btn);
+    }
+  });
+}
+
+document.addEventListener('turbolinks:load', function(e) {
+  tryInsertButton(e.data.url);
+});
